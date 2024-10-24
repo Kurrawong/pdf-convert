c26587f53ff6b5b984c9284f0fca097133395213
Lawon Lewis <lawson@kurrawong.ai>
2024-10-24 12:24:48 +1000

    handle duplicate names
    files with same name (but different extension) were being overriden

--- convert-to-pdf.sh
+++ convert-to-pdf.sh
@@ -6,11 +6,18 @@
 
 # Loop over all files in the /data directory
 for file in /data/*; do
-  # Check if the file is not a PDF
-  if [ "${file##*.}" != "pdf" ]; then
-    # Convert the file to PDF
-    libreoffice --headless --convert-to pdf "$file" --outdir "/data"
-  fi
+	# Check if the file is not a PDF
+	if [ "${file##*.}" != "pdf" ]; then
+		# Extract the filename with extension
+		filename_ext=$(basename "$file")
+		filename_no_ext=$(basename "$file" | cut -d. -f1)
+
+		# Convert the file to PDF
+		libreoffice --headless --convert-to pdf "$file" --outdir "/data"
+
+		# Rename it
+		mv "/data/${filename_no_ext}.pdf" "/data/${filename_ext}.pdf"
+	fi
 done
 
 
